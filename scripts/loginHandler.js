6c855b8167701332f26ac209cc3f2a1153ed9419
clambe <camden.lambe@wsu.edu>
2020-01-29 17:27:27 -0800

    added the login functionality

--- scripts/loginHandler.js
+++ scripts/loginHandler.js
@@ -1,16 +1,19 @@
+var curUser = null;
+var loggedIn = false;
 function validateLogin() {
     let validEmail = false;
     let validPassword = false;
-    let emailRGEX = new RegExp("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+    let emailRGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     let passwordRGEX = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
 
     //check email
     let email = document.getElementById('emailInput');
-    validEmail = emailRGEX.test(email.nodeValue);
+    validEmail = emailRGEX.test(email.value);
+    console.log(email);
     console.log("validEmail:" + validEmail);
     //check password
     let password = document.getElementById('passwordInput');
-    validPassword = passwordRGEX.test(password.nodeValue);
+    validPassword = passwordRGEX.test(password.value);
     console.log("validPassword:" + validPassword);
 
     //alert if wrong
@@ -19,6 +22,10 @@ function validateLogin() {
         alert("move to next page");
         console.log("move to next page");
         document.getElementById('loginPageDiv').style.display = 'none';
+        loggedIn = true;
+        curUser = email.value;
+        email.value = "";
+        password.value = "";
     }
     else if (validEmail && !validPassword){
         //incorrect password
