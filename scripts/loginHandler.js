a51f22fe156adc4cfca095e0a0f44368524b95ab
clambe <camden.lambe@wsu.edu>
2020-01-29 18:13:41 -0800

    added the fidget spinner button thing. reduced time and bypassed login for ease of testing

--- scripts/loginHandler.js
+++ scripts/loginHandler.js
@@ -1,11 +1,11 @@
 var curUser = null;
 var loggedIn = false;
+
 function validateLogin() {
     let validEmail = false;
     let validPassword = false;
     let emailRGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     let passwordRGEX = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-
     //check email
     let email = document.getElementById('emailInput');
     validEmail = emailRGEX.test(email.value);
@@ -15,28 +15,39 @@ function validateLogin() {
     let password = document.getElementById('passwordInput');
     validPassword = passwordRGEX.test(password.value);
     console.log("validPassword:" + validPassword);
+    document.getElementById('loginFormButton').classList.add("fas", 'fa-spinner', 'fa-spin');
 
-    //alert if wrong
-    if(validEmail && validPassword){
-        //move to next page
-        alert("move to next page");
-        console.log("move to next page");
-        document.getElementById('loginPageDiv').style.display = 'none';
-        loggedIn = true;
-        curUser = email.value;
-        email.value = "";
-        password.value = "";
-    }
-    else if (validEmail && !validPassword){
-        //incorrect password
-        alert("password is too weak.");
-    }
-    else if (!validEmail && validPassword){
-        //incorrect email
-        alert("email is not valid.")
-    }
-    else {
-        //both are wrong
-        alert("email is not valid and password is too weak");
-    }
+    setTimeout(() => {
+        document.getElementById('loginFormButton').classList.remove("fas", 'fa-spinner', 'fa-spin');
+
+        validEmail = true;
+        validPassword = true;
+
+        //alert if wrong
+        if (validEmail && validPassword) {
+            //move to next page
+            //alert("move to next page");
+            console.log("move to next page");
+            //add spinner for no reason
+
+
+            document.getElementById('loginPageDiv').style.display = 'none';
+            loggedIn = true;
+            curUser = email.value;
+            email.value = "";
+            password.value = "";
+        }
+        else if (validEmail && !validPassword) {
+            //incorrect password
+            alert("password is too weak.");
+        }
+        else if (!validEmail && validPassword) {
+            //incorrect email
+            alert("email is not valid.")
+        }
+        else {
+            //both are wrong
+            alert("email is not valid and password is too weak");
+        }
+    }, 1);
 }
